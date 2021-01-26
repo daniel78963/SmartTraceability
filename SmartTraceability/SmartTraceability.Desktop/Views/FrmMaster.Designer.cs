50bf8b31a1fa74227e4f3fa4da385d89c4c6b66c
Daniel Alejandro Ramirez Ramirez <daniel78963@hotmail.com>
2021-01-25 19:42:59 -0500

    add frmMaster

---
+++ SmartTraceability/SmartTraceability.Desktop/Views/FrmMaster.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace SmartTraceability.Desktop.Views
+{
+    partial class FrmMaster
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlMenu = new System.Windows.Forms.Panel();
+            this.pnlLogo = new System.Windows.Forms.Panel();
+            this.lblLogo = new System.Windows.Forms.Label();
+            this.pnlPageTitle = new System.Windows.Forms.Panel();
+            this.pnlDesktop = new System.Windows.Forms.Panel();
+            this.pnlMenu.SuspendLayout();
+            this.pnlLogo.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlMenu
+            // 
+            this.pnlMenu.BackColor = System.Drawing.Color.White;
+            this.pnlMenu.Controls.Add(this.pnlLogo);
+            this.pnlMenu.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlMenu.Location = new System.Drawing.Point(0, 0);
+            this.pnlMenu.Name = "pnlMenu";
+            this.pnlMenu.Size = new System.Drawing.Size(800, 80);
+            this.pnlMenu.TabIndex = 0;
+            // 
+            // pnlLogo
+            // 
+            this.pnlLogo.Controls.Add(this.lblLogo);
+            this.pnlLogo.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnlLogo.Location = new System.Drawing.Point(0, 0);
+            this.pnlLogo.Name = "pnlLogo";
+            this.pnlLogo.Size = new System.Drawing.Size(241, 80);
+            this.pnlLogo.TabIndex = 0;
+            // 
+            // lblLogo
+            // 
+            this.lblLogo.AutoSize = true;
+            this.lblLogo.Font = new System.Drawing.Font("Segoe UI Light", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblLogo.Location = new System.Drawing.Point(12, 24);
+            this.lblLogo.Name = "lblLogo";
+            this.lblLogo.Size = new System.Drawing.Size(191, 32);
+            this.lblLogo.TabIndex = 0;
+            this.lblLogo.Text = "Smart Traceability";
+            // 
+            // pnlPageTitle
+            // 
+            this.pnlPageTitle.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlPageTitle.Location = new System.Drawing.Point(0, 80);
+            this.pnlPageTitle.Name = "pnlPageTitle";
+            this.pnlPageTitle.Size = new System.Drawing.Size(800, 23);
+            this.pnlPageTitle.TabIndex = 1;
+            // 
+            // pnlDesktop
+            // 
+            this.pnlDesktop.BackColor = System.Drawing.Color.White;
+            this.pnlDesktop.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlDesktop.Location = new System.Drawing.Point(0, 103);
+            this.pnlDesktop.Name = "pnlDesktop";
+            this.pnlDesktop.Size = new System.Drawing.Size(800, 347);
+            this.pnlDesktop.TabIndex = 2;
+            // 
+            // FrmMaster
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pnlDesktop);
+            this.Controls.Add(this.pnlPageTitle);
+            this.Controls.Add(this.pnlMenu);
+            this.Name = "FrmMaster";
+            this.Text = "Smart Traceability";
+            this.pnlMenu.ResumeLayout(false);
+            this.pnlLogo.ResumeLayout(false);
+            this.pnlLogo.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlMenu;
+        private System.Windows.Forms.Panel pnlPageTitle;
+        private System.Windows.Forms.Panel pnlDesktop;
+        private System.Windows.Forms.Panel pnlLogo;
+        private System.Windows.Forms.Label lblLogo;
+    }
+}
---
+++ SmartTraceability/SmartTraceability.Desktop/Views/FrmMaster.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SmartTraceability.Desktop.Views
+{
+    public partial class FrmMaster : Form
+    {
+        public FrmMaster()
+        {
+            InitializeComponent();
+        }
+    }
+}
